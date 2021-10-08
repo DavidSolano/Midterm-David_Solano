875e7d02ce922cbe064736b4515dc1ca26676837
David Solano <dsolanoreyes@my.wctc.edu>
2021-10-07 19:56:30 -0500

    Initial commit

---
+++ RaceTrack/Cars/FordFiesta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class FordFiesta : RaceCar
+    {
+        public FordFiesta()
+        {
+            Name = "Ford Fiesta";
+            TopSpeed = 250;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"the {Name} is ready to start the party");
+            Console.WriteLine("get it?");
+            Console.WriteLine("cause fiesta means party.. in spanish...");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"{Name} struggles to stop. The party is simply too strong!");
+            base.Brake();
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Billy.cs
@@ -0,0 +1,18 @@
+﻿using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Billy : Driver
+    {
+        public Billy(RaceCar car) : base(car)
+        {
+            Name = "Billy";
+            SkillLevel = 4;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
